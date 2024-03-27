2fc85d284002806126efff7d65640545b045ece6
Haojie Zhang <zhj3408237@gmail.com>
2024-03-26 23:35:12 -0400

    Portfolio Completed

---
+++ src/components/About.jsx
@@ -0,0 +1,32 @@
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+              About
+            </p>
+          </div>
+          <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-4xl font-bold">
+            <p>Hi. I'm RJ, nice to meet you. Please take a look around.</p>
+          </div>
+          <div>
+            <p>
+              As a front-end developer, I'm driven by a passion for building
+              clean, user-friendly interfaces. I thrive on tackling challenges
+              head-on and constantly seek opportunities to learn and grow in
+              this dynamic field. Let's bring creativity and innovation to the
+              table together!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+import GithubImg from "../assets/projects/github-app.png";
+import FitnessImg from "../assets/projects/fitness-app.png";
+import ExpenseImg from "../assets/projects/expense-tracker.png";
+import AiImg from "../assets/projects/summarizer-app.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "Github Repo App",
+    image: GithubImg,
+    github: "https://github.com/HaojieZhang7/repository-app",
+    live: "https://mern-github-app-8vpd.onrender.com",
+  },
+  {
+    id: 2,
+    name: "Fitness Exercises App",
+    image: FitnessImg,
+    github: "https://github.com/HaojieZhang7/FitnessWebApp",
+    live: "https://helpful-kashata-6ebad4.netlify.app",
+  },
+  {
+    id: 3,
+    name: "Expense Tracker",
+    image: ExpenseImg,
+    github: "https://github.com/HaojieZhang7/expense-tracker-firebase-react",
+    live: "https://expense-tracker1-38abe.web.app",
+  },
+  {
+    id: 4,
+    name: "Article Summarizer",
+    image: AiImg,
+    github: "https://github.com/HaojieZhang7/Article-Summarizer",
+    live: "https://sunny-dolphin-1642c1.netlify.app",
+  },
+];
