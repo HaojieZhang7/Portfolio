283c79514ce727ff6398413f75728f4615e217c3
Haojie Zhang <zhj3408237@gmail.com>
2024-03-27 23:57:58 -0400

    navbar link updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -134,7 +134,7 @@ const Navbar = () => {
             <a
               className="flex justify-between items-center w-full text-gray-300"
               target="_blank"
-              href="https://drive.google.com/file/d/1gse-fGEmdENg39ZCicL020hoIH40hKUo/view?usp=sharing"
+              href="https://drive.google.com/file/d/1-yCx63YQx_zdrOvpeW6e0dhSPDbi8Lpg/view?usp=sharing"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
